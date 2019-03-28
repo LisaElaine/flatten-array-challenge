24918270989d3766702a4938ed711fd1c5aa8562
Lisa Conti <leconti2@gmail.com>
2019-03-28 14:03:08 +1300

    write tests

--- spec/flatten_array_spec.rb
+++ spec/flatten_array_spec.rb
@@ -1,1 +1,20 @@
+require 'flatten_array'
 
+describe '#flatten_array' do
+  it 'should return message if argument passed is not an array' do
+    array = flatten_array('Hey Flick Team!')
+    expect(array).to eq 'argument passed is not an array'
+  end
+
+  it 'should return false if array contains non-array objects / non-integers' do
+    array = flatten_array([1, 2, 3, { 9 => [1, 2, 3, 4, 5, 6, 7, 8, 9] }])
+    expect(array).to eq false
+  end
+
+  it 'should return a flat array when passed an array of arbitrarily nested arrays of integers' do
+    array = flatten_array([[1, 2, [3]], 4])
+    array2 = flatten_array([6, 7, [[1, 2, [3]], 4]])
+    expect(array).to eq [1, 2, 3, 4]
+    expect(array2).to eq [6, 7, 1, 2, 3, 4]
+  end
+end
